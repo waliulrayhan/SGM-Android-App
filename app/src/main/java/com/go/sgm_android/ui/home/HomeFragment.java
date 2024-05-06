eb158a5b74afa7000195bd494519a701c63c2e0c
Md. Waliul Islam Rayhan <b190305034@cse.jnu.ac.bd>
2024-05-07 00:56:17 +0600

    Add Comment Successful.

--- app/src/main/java/com/go/sgm_android/ui/home/HomeFragment.java
+++ app/src/main/java/com/go/sgm_android/ui/home/HomeFragment.java
@@ -1,19 +1,21 @@
 package com.go.sgm_android.ui.home;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.telephony.SignalStrength;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.go.sgm_android.Adapter.DistributorAdapter;
 import com.go.sgm_android.Adapter.PowerPlantAdapter;
@@ -21,9 +23,11 @@ import com.go.sgm_android.AddPowerPlantActivity;
 import com.go.sgm_android.DistributorListActivity;
 import com.go.sgm_android.MainActivity;
 import com.go.sgm_android.PowerPlantListActivity;
+import com.go.sgm_android.R;
 import com.go.sgm_android.databinding.FragmentHomeBinding;
-import com.go.sgm_android.model.Distributor;
 import com.go.sgm_android.model.PowerPlant;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -31,11 +35,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
@@ -55,6 +58,8 @@ public class HomeFragment extends Fragment {
 
         final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        fetchDataFromFirebase();
 
         //==========================================================================================
         // This is for Time and Date
@@ -104,6 +109,57 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        binding.addCentralCommand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Inflate the dialog layout
+                View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.dialog_layout, null);
+
+                // Find views in the dialog layout
+                EditText editTextInput = dialogView.findViewById(R.id.editTextInput);
+                Button buttonSave = dialogView.findViewById(R.id.buttonSave);
+                Button buttonCancel = dialogView.findViewById(R.id.buttonCancel);
+
+                // Create the dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                builder.setView(dialogView);
+                AlertDialog dialog = builder.create();
+
+                // Set click listener for Save button
+                buttonSave.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Get input from EditText
+                        String inputData = editTextInput.getText().toString().trim();
+
+                        // Handle saving logic here
+                        // For example, you can save data to Firebase or perform other actions
+
+                        if (!inputData.isEmpty()){
+                            Toast.makeText(getContext(), "Hello "+inputData, Toast.LENGTH_SHORT).show();
+                            uploadCentralCommandToFirebase(inputData);
+                        }
+
+                        // Dismiss the dialog
+                        dialog.dismiss();
+                    }
+                });
+
+                // Set click listener for Cancel button
+                buttonCancel.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // Dismiss the dialog without saving
+                        dialog.dismiss();
+                    }
+                });
+
+                // Show the dialog
+                dialog.show();
+            }
+        });
+
+
 //        //==========================================================================================
 //        // This is for Power Plant Recycler View
 //        // Initialize RecyclerView
@@ -137,6 +193,107 @@ public class HomeFragment extends Fragment {
 //        fetchTotalData();
 
         return root;
+    }
+
+    private void uploadCentralCommandToFirebase(String inputData) {
+        // Get the current date
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+        String currentDate = dateFormat.format(new Date());
+
+        // Reference to "SGM/Date/currentDate/comments"
+        DatabaseReference commentRef = FirebaseDatabase.getInstance()
+                .getReference()
+                .child("SGM")
+                .child("Date")
+                .child(currentDate)
+                .child("comments");
+
+        // Push the comment to generate a unique key
+        String commentKey = commentRef.push().getKey();
+
+        // Create a map to hold the comment data
+        Map<String, Object> commentData = new HashMap<>();
+        commentData.put("comment", inputData);
+
+        // Upload the comment to Firebase
+        if (commentKey != null) {
+            commentRef.child(commentKey).setValue(commentData)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            // Comment uploaded successfully
+                            Log.d("UploadCentralCommand", "Comment uploaded successfully");
+                            // You can add any additional actions you want to perform on success
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            // Failed to upload comment
+                            Log.e("UploadCentralCommand", "Failed to upload comment: " + e.getMessage());
+                            // Handle the error, if needed
+                        }
+                    });
+        }
+    }
+
+
+    private void fetchDataFromFirebase() {
+        // Get the current date
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+        String currentDate = dateFormat.format(new Date());
+
+        // Construct the Firebase reference path for the current date
+        DatabaseReference ppReference = FirebaseDatabase.getInstance().getReference()
+                .child("SGM").child("Date").child(currentDate);
+        ppReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Retrieve total node
+                    DataSnapshot totalsSnapshot = dataSnapshot.child("total");
+
+                    String totalCurrentCapacityValue = totalsSnapshot.child("AllppcurrentCapacity").getValue(String.class);
+
+                    // Update UI with fetched values
+                    binding.ppTotalCurrentCapacity.setText("Total Current Capacity\n"+String.valueOf(totalCurrentCapacityValue)+" MW");
+                } else {
+                    // Handle case when data doesn't exist
+                    // You can display a message or take appropriate action
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle onCancelled
+            }
+        });
+
+        // Construct the Firebase reference path for the current date
+        DatabaseReference ddReference = FirebaseDatabase.getInstance().getReference()
+                .child("SGM").child("Date").child(currentDate);
+        ppReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    // Retrieve total node
+                    DataSnapshot totalsSnapshot = dataSnapshot.child("total");
+
+                    String totalCurrentCapacityValue = totalsSnapshot.child("AllddcurrentDemand").getValue(String.class);
+
+                    // Update UI with fetched values
+                    binding.ddTotalCurrentDemand.setText("Total Current Demand\n"+String.valueOf(totalCurrentCapacityValue)+" MW");
+                } else {
+                    // Handle case when data doesn't exist
+                    // You can display a message or take appropriate action
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle onCancelled
+            }
+        });
     }
 
 //    private void fetchTotalData() {
